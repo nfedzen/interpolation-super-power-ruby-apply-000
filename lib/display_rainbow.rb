c4736d5fbb2041cf7f93482860d373eb65c603d9
Nathan Fedzen <nfedzen@hotmail.com>
2020-10-13 21:11:26 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
 # Write your #display_rainbow method here
-def display_rainbow(rainbow[])
+def display_rainbow(rainbow)
   puts "R: #{raibow[0]}, O: #{raibow[1]}, Y: #{raibow[2]}, G: #{raibow[3]}, B: #{raibow[4]}, I: #{raibow[5]}, V: #{raibow[6]}"
 end
